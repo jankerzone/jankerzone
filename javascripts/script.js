020b298992351bebb1271432f8056c4a6856d419
Robsan <silentjanker@gmail.com>
2016-03-31 14:57:41 +0700

    Rombak total

---
+++ javascripts/script.js
@@ -0,0 +1,36 @@
+	// create the module and name it scotchApp
+	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+	// configure our routes
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/hello.html',
+				controller  : 'mainController'
+			})
+		
+			.when('/webkamu', {
+				templateUrl : 'pages/2-web-kamu.html',
+				controller  : 'webkamuController'
+			})
+
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
+			})
+
+			// route for the contact page
+			.when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactController'
+			});
+	});
+
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'TEst!';
+	});
--- javascripts/utama.js
+++
@@ -1,7 +0,0 @@
-function searchPage($scope){
-    $scope.searchResults = [{label:"BBC",_infoLink:"http://www.bbc.co.uk/"},{label:"CNN",_infoLink:"http://edition.cnn.com/"}];
-    
-    $scope.itemDetail = function(link){
-        $scope.detailFrame = link;
-    };
-}
